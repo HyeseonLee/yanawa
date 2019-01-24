728ebf3cf6504541ca419f41899cfe78024b5582
holywater <holywater@gist.ac.kr>
2019-01-24 18:34:20 +0900

    [punishment_roulletting.js] release ver. 0.1

---
+++ seongsu_dev/punishment_rouletting.js
@@ -0,0 +1,35 @@
+// 벌칙 리스트 변수
+var punishment_list = []
+// 기본 벌칙들
+var default = ['벌칙1','벌칙2','벌칙3','벌칙4','벌칙5'] //FIXME
+
+punishment_list = copy.default
+
+// 랜덤한 숫자 인덱스를 생성해서 벌칙리스트에서 벌칙을 반환
+function rouletting_punishment() {
+    var ran_num = Math.floor((Math.random() * 100) + 1)%(punishment_list.length);
+    // https://www.w3schools.com/jsref/jsref_random.asp
+    return punishment_list[ran_num]
+}
+
+// HTML 구문에서 벌칙 text 변경
+function change_punishment_text() {
+
+}
+
+// 룰렛버튼 작동 구문
+var roullet_Button = null //FIXME
+var roullet_text_HTML = null //FIXME
+var transition_time = 10s //FIXME
+var clicked = false
+
+roullet_Button.addEventListener('click', function() {
+    if(!clicked){
+        clicked = true;
+        //클릭되자 말자 HTML 구문의 텍스트 삭제
+        roullet_text_HTML = null //FIXME
+        document.body.classList.toggle('roullet_Button_clicked');
+        delay(transition_time)
+        roullet_text_HTML = roulleting_punishment()
+    }
+})
