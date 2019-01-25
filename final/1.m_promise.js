bce36a51b4c8cd51f381a0d6b438dec437f0b701
holywater <holywater@gist.ac.kr>
2019-01-25 14:56:54 +0900

    [1.m_promise.js] 수정

--- final/1.m_promise.js
+++ final/1.m_promise.js
@@ -93,9 +93,17 @@ button_yes.addEventListener('click', function() {
 		alert("이름을 입력하세요")
 		return;
 	}
+
+	var d = inputed_time;
+	var inputed_time2 = d.getFullYear() +"년 " + (d.getMonth() + 1) + "월 " + d.getDate() + "일 " + d.getHours() + "시 " + d.getMinutes() + "분"
 	// 현재 지도의 마킹 위치를 체크하고 있는지 확인
-	var temp = "\n약속 이름 : " + pms_name + "\n약속 시간 : " + inputed_time;
-	alert(temp);
+	var temp = "\n약속 이름 : " + pms_name + "\n약속 시간 : " + inputed_time2;
+	var retVal = confirm(temp + "\n위의 설정이 맞습니까?")
+	if(retVal) {
+		//파싱할 정보 있음
+		window.location.href="";
+		window.open()
+	}
 })
 
 
